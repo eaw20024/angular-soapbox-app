47d671057a120ebda0bfa6255029d1b152fb91bb
Eric Washington <ericwashington@erics-mbp.eia.doe.gov>
2018-03-14 11:13:42 -0400

    Soapbox Services

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { DataService } from '../../services/data.service';
 import { User } from '../../models/User'; 
 
 @Component({
@@ -18,56 +18,31 @@ export class UsersComponent implements OnInit {
   loaded: boolean = false;
   enableAdd: boolean = false;
   showUserForm: boolean = false;
+  @ViewChild('userForm') form: any;
 
-  constructor() { }
+  constructor(private dataService: DataService) {
+
+   }
 
   ngOnInit() {
-   
-      this.users = [
-        {
-          firstName: 'John',
-          lastName: 'Doe',
-          email: 'john@gmail.com',
-          isActive: true,
-          registered: new Date('01/02/2018 08:30:00'),
-          hide: true
-        },
-        {
-          firstName: 'Kevin',
-          lastName: 'Johnson',
-          email: 'kevin@yahoo.com',
-          isActive: false,
-          registered: new Date('03/11/2017 06:20:00'),
-          hide: true
-        },
-        {
-          firstName: 'Karen',
-          lastName: 'Williams',
-          email: 'karen@gmaial.com',
-          isActive: true,
-          registered: new Date('11/02/2016 10:30:00'),
-          hide: true
-        }
-      ];
+    
+      this.users = this.dataService.getUsers();
 
       this.loaded = true;
   }
 
-  // addUser() {
-  //   this.user.isActive = true;
-  //   this.user.registered = new Date();
+  onSubmit({value, valid}: {value: User, valid: boolean}) {
+    if(!valid){
+       console.log('Form is not valid!')
+    } else {
+      value.isActive = true;
+      value.registered = new Date();
+      value.hide = true;
 
-  //   this.users.unshift(this.user);
+      this.dataService.addUser(value);
 
-  //   this.user = {
-  //     firstName: '',
-  //     lastName: '',
-  //     email: ''
-  //   }
-  // }
-
-  onSubmit() {
-
+      this.form.reset();
+    }
   }
 
   
